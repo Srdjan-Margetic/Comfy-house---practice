8abb396822c0e5a1af333110f99d38c0389fc351
srdjan_margetic <srdjan1983@live.com>
2020-04-29 11:13:53 +0200

    cart value(price) and total amount of items in the bag added

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
 // cart
 let cart =[];
+// buttons
+let buttonsDOM = [];
 // getting the products
 class Products {
     async getProducts() {
@@ -57,19 +59,43 @@ class UI {
     }
     getBagButtons() {
         const buttons = [...document.querySelectorAll('.bag-btn')];
+        buttonsDOM = buttons;
         buttons.forEach(button => {
             let id = button.dataset.id;
             let inCart = cart.find(item => item.id === id);
             if(inCart) {
                 button.innerText = "In Cart";
                 button.disabled = true;
-            } else {
-                button.addEventListener('click', (event) => {
-                    event.target.innerText = "inCart";
-                    event.target.disabled = true
-                })
             }
-        })
+            button.addEventListener('click', (event) => {
+                event.target.innerText = "inCart";
+                event.target.disabled = true;
+                // get product from products and add the amound of item
+                let cartItem ={...Storage.getProduct(id), amount:1};
+                // add product to the cart
+                cart = [...cart, cartItem];
+                // save cart in local storage
+                Storage.saveCart(cart);
+                // set cart values
+                this.setCartValues(cart);
+                // add cart item
+                // display cart item
+                // show the cart
+                
+            });
+        });
+    }
+    setCartValues(cart) {
+        let tempTotal = 0;
+        let itemsTotal = 0;
+        cart.map(item => {
+            tempTotal += item.price * item.amount;
+            itemsTotal += item.amount;
+        });
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+        console.log(cartTotal);
+        console.log(cartItems);
     }
 };
 
@@ -77,6 +103,14 @@ class UI {
 class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products));
+    }
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === id);
+    }
+
+    static saveCart(cart) {
+        localStorage.setItem('cart', JSON.stringify(cart));
     }
 };
 
